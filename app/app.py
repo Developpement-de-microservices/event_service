1d17cdb5351ddcb4bcd943c77e614141fcc67f6f
Esteban Heschung <esteban.heschung@etudiant.univ-rennes.fr>
2026-03-31 18:41:05 +0200

    Changement image vers alpine, migration vers MongoDB

--- app/app.py
+++ app/app.py
@@ -1,130 +1,109 @@
 from flask import Flask, request, jsonify
 from datetime import datetime, timezone
-from flask_cors import CORS #pour try sur swagger
+from flask_cors import CORS
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 import requests
-import uuid
-import json
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-def load_events():
-    try:
-        with open("./data.json", "r") as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {}
-
-def save_events(events):
-    with open("./data.json", "w") as file:
-        json.dump(events, file)
+client = MongoClient(os.environ["MONGO_URI"]) #connexion mongoDB
+db = client["events_db"]
+events_collection = db["events"]
 
 def check_id_exists(service_url, id_value, service, token):
     headers = {"Authorization": f"Bearer {token}"}
     try:
-        resp = requests.get(service_url+id_value,headers=headers)
+        resp = requests.get(service_url + id_value, headers=headers)
         if resp.status_code != 200:
             return False, f"{service} ID does not exist"
         return True, None
     except requests.RequestException:
         return False, f"Service communication issue, check {service} API"
 
-@app.route("/deployments/<deployment_id>/events", methods=["GET"])
-def events_deployement(deployment_id):
-    token = request.headers.get("Authorization", "").replace("Bearer ", "")
+def verify_token(token):
     try:
         headers = {"Authorization": f"Bearer {token}"}
         response = requests.post("http://proxy/auth/verify", headers=headers)
         if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
+            return False
+        return True
     except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+        return False
+
+@app.route("/deployments/<deployment_id>/events", methods=["GET"])
+def events_deployment(deployment_id):
+    token = request.headers.get("Authorization", "").replace("Bearer ", "")
+    if not verify_token(token):
+        return jsonify({"error": "Not authorized"}), 401
     
-    events = load_events()
-    deployment_events = []
-
-    for event in events.values():
-        if event["deploymentId"] == deployment_id:
-            deployment_events.append(event)
-
+    deployment_events = list(events_collection.find({"deploymentId": deployment_id}, {"_id": 0})) #on recherche par déploiement_id sans récupérer l'id mongoDB
     return jsonify(deployment_events), 200
 
 @app.route("/events", methods=["GET"])
 def list_events():
     token = request.headers.get("Authorization", "").replace("Bearer ", "")
-    try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+    if not verify_token(token):
+        return jsonify({"error": "Not authorized"}), 401
     
-    events = load_events()
-    return jsonify(list(events.values())), 200 #retourne notre dictionnaire sous forme de json -> passage par liste obligé
+    all_events = list(events_collection.find({}, {"_id": 0}))
+    return jsonify(all_events), 200
 
 @app.route("/events", methods=["POST"])
 def create_event():
     token = request.headers.get("Authorization", "").replace("Bearer ", "")
-    try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+    if not verify_token(token):
+        return jsonify({"error": "Not authorized"}), 401
     
-    events = load_events()
     data = request.json
-
     required = ["deploymentId", "type", "message"]
     for elem in required:
-        if not elem in data:
+        if elem not in data:
             return jsonify({"message": "Missing required field"}), 400
-        
+
     types = ["DEPLOYMENT_STARTED", "DEPLOYMENT_FINISHED", "DEPLOYMENT_ERROR", "ROLLBACK"]
-    if not data["type"] in types:
+    if data["type"] not in types:
         return jsonify({"message": "Invalid type"}), 400
-    
+
     ok, msg = check_id_exists("http://proxy/deployments/", data["deploymentId"], "Deployment", token)
-    if not ok: #id non existant ou service non attegnable
+    if not ok:
         return jsonify({"message": msg}), 400
 
-    if data.get("initiatedBy"): #argument faculatif
+    if data.get("initiatedBy"):
         ok, msg = check_id_exists("http://proxy/users/", data["initiatedBy"], "User", token)
         if not ok:
             return jsonify({"message": msg}), 400
 
-    event_id = str(uuid.uuid4()) #nombre aléatoire
-    
     event = {
-        "id": event_id,
         "deploymentId": data["deploymentId"],
         "type": data["type"],
         "message": data["message"],
-        "data": data.get("data", {}), #si vide alors {}
+        "data": data.get("data", {}),
         "initiatedBy": data.get("initiatedBy", ""),
         "createdAt": datetime.now(timezone.utc).isoformat()
-    } #dico d'event
+    }
 
-    events[event_id] = event # dico pour ne pas a faire de boucle for lors des get
-    save_events(events)
+    result = events_collection.insert_one(event)
+    event["id"] = str(result.inserted_id)  #on met une entrée id avec l'id de mongoDB
     return jsonify(event), 201
 
 @app.route("/events/<event_id>", methods=["GET"])
 def get_event(event_id):
     token = request.headers.get("Authorization", "").replace("Bearer ", "")
+    if not verify_token(token):
+        return jsonify({"error": "Not authorized"}), 401
+
     try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
-    
-    events = load_events()
-    if event_id in events:
-        return jsonify(events[event_id]), 200
+        event = events_collection.find_one({"_id": ObjectId(event_id)}) #conversion en type mongoDB pour son ID
+    except:
+        return jsonify({"message": "Invalid event ID format"}), 400
+
+    if event:
+        event["id"] = str(event["_id"])
+        del event["_id"]
+        return jsonify(event), 200
     
     return jsonify({"message": "Event not found"}), 404
 
